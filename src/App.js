673524f438459694e144d97bbb82ce7c33130501
ernest korir <ernestbore3@gmail.com>
2023-02-16 20:29:05 +0300

    Fix: Fix linters

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+import Calculator from './component/Calculator';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <>
+        <Calculator />
+      </>
+    );
+  }
+}
+export default App;
