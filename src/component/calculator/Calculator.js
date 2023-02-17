d03ac50e05492a8b4ef70806c76527b93dc568be
ernest korir <ernestbore3@gmail.com>
2023-02-17 11:27:38 +0300

    Add logic to calculator

--- src/component/calculator/Calculator.js
+++ src/component/calculator/Calculator.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import Button from '../Button';
+import calculate from '../logic/calculate';
+import './Calculator.css';
 
 const operators = ['÷', 'x', '-', '+', '='];
 const digits = ['AC', '+/-', '%', '7', '8', '9', '4', '5', '6', '1', '2', '3', '0', '.'];
@@ -7,16 +8,38 @@ const digits = ['AC', '+/-', '%', '7', '8', '9', '4', '5', '6', '1', '2', '3', '
 class Calculator extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      next: '',
+      total: '',
+      operation: '',
+    };
+  }
+
+  handleClick = (btn) => {
+    this.setState((state) => calculate(
+      {
+        next: state.next,
+        total: state.total,
+        operation: state.operation,
+      },
+      btn,
+    ));
   }
 
   render() {
+    const { total, next } = this.state;
     return (
       <ul className="calc-body">
-        <li className="calc-screen">0</li>
+        <li className="calc-screen">
+          {next || total || '0'}
+        </li>
         <li className="calc-keyboard">
-          <Button buttons={digits} />
-          <Button buttons={operators} />
+          <div className="calc-digits">
+            {digits.map((btn) => <button onClick={() => this.handleClick(btn)} type="button" key={btn}>{ btn }</button>) }
+          </div>
+          <div className="calc-operators">
+            {operators.map((btn) => <button onClick={() => this.handleClick(btn)} type="button" key={btn}>{ btn }</button>) }
+          </div>
         </li>
       </ul>
     );
